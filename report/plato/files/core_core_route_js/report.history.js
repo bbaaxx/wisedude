5724ef61a0d314fed400421366000bd848c7e0e6
Eduardo Mosqueda <emosquedach@gmail.com>
2016-08-29 19:35:06 -0500

    before merging with partial c9

--- report/plato/files/core_core_route_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Wed, 20 Jan 2016 03:17:58 GMT","sloc":26,"lloc":14,"functions":3,"deliveredBugs":0.08012258203429339,"maintainability":77.11394883138928,"lintErrors":[],"difficulty":6.63157894736842}]
